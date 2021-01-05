daa730f3c9fa73dcec294cd8316115a26e27f90d
BaeGyoungYoon <qoruddbs8911@naver.com>
2021-01-05 23:21:59 +0900

    2021-01-05 코드업   ㅋ노드업 풀이

---
+++ 코드업/1011.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    char x;
+    scanf("%c", &x);
+    printf("%c", x);
+
+    return 0;
+}
---
+++ 코드업/1020.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a,b;
+    scanf_s("%d-%d", &a, &b );
+    printf("%06d%07d", a, b);
+
+    return 0;
+}
